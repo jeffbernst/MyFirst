a3cd134a0f59bf6aee8d1067dece61f9425dc72d
Jeff Bernstein <jeffreybbernstein@gmail.com>
2024-07-26 16:01:41 -0700

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Jeff Bernstein on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
